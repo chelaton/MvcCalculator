58a78b3f13fe1deeb0c8f18a6597facd64b0639a
chelaton <chelaton@seznam.cz>
2022-06-05 16:16:26 +0200

    migration

---
+++ Data/Migrations/20220605141446_calchistory.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class calchistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalcHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MathFormula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Result = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalcHistory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MathOperator",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OperationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MathOperator", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "MathOperator",
+                columns: new[] { "Id", "OperationName", "Symbol" },
+                values: new object[,]
+                {
+                    { 1, "Scitani", "+" },
+                    { 2, "Odcitani", "-" },
+                    { 3, "Nasobeni", "*" },
+                    { 4, "Deleni", "/" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalcHistory");
+
+            migrationBuilder.DropTable(
+                name: "MathOperator");
+        }
+    }
+}
